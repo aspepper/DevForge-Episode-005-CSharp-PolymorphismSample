905a5488872e0cb66a0732b899eff2561b1a005b
Alex Pimenta <aspepper@hotmail.com>
2024-02-25 15:58:46 -0300

    first commit

---
+++ CSharp-Simple-Sample/Program.cs
@@ -0,0 +1,40 @@
+﻿class Animal
+{
+    public virtual void EmitSound()
+    {
+        Console.WriteLine("Animal making noise");
+    }
+}
+
+class Dog : Animal
+{
+    public override void EmitSound()
+    {
+        Console.WriteLine("Woo wow!");
+    }
+}
+
+class Cat : Animal
+{
+    public override void EmitSound()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
+
+class Program
+{
+    static void MakeNoise(Animal animal)
+    {
+        animal.EmitSound();
+    }
+
+    static void Main()
+    {
+        Animal animal1 = new Dog();
+        Animal animal2 = new Cat();
+
+        MakeNoise(animal1);
+        MakeNoise(animal2);
+    }
+}
